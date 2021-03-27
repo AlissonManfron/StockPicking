0dcae08c89965fb76bf67d424f5c221f978b1897
Alisson Manfron <alisson.manfron@bradesco.com.br>
2021-03-27 17:47:14 -0300

    - Adicionado o gráfico da tela de "Resumo".

--- app/src/main/java/br/com/alisson/stockpicking/ui/resume/ResumeFragment.kt
+++ app/src/main/java/br/com/alisson/stockpicking/ui/resume/ResumeFragment.kt
@@ -1,31 +1,78 @@
 package br.com.alisson.stockpicking.ui.resume
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import br.com.alisson.stockpicking.R
+import br.com.alisson.stockpicking.data.db.AppDatabase
+import br.com.alisson.stockpicking.data.repository.StockDbDataSource
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.MPPointF
+
 
 class ResumeFragment : Fragment() {
 
-    private lateinit var viewModel: ResumeViewModel
+    private val viewModel: ResumeViewModel by activityViewModels(
+        factoryProducer = {
+            val dataBase = AppDatabase.getDatabase(requireContext())
+
+            ResumeViewModel.ResumeViewModelFactory(
+                stockRepository = StockDbDataSource(dataBase.stockDao())
+            )
+        }
+    )
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        viewModel =
-                ViewModelProviders.of(this).get(ResumeViewModel::class.java)
+
         val root = inflater.inflate(R.layout.fragment_resume, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        viewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+
+        val pieChartView = root.findViewById<PieChart>(R.id.pieChartView)
+
+        viewModel.getEntryList().observe(viewLifecycleOwner, Observer { pieList ->
+
+            val pieDataSet = PieDataSet(pieList, "")
+            pieDataSet.setDrawIcons(false)
+            pieDataSet.sliceSpace = 3f
+            pieDataSet.iconsOffset = MPPointF(0F, 40F)
+            pieDataSet.selectionShift = 5f
+            pieDataSet.colors = viewModel.colors
+
+            val pieData = PieData(pieDataSet)
+            pieData.setValueFormatter(PercentFormatter())
+            pieData.setValueTextSize(11f)
+            pieData.setValueTextColor(Color.WHITE)
+
+            pieChartView.data = pieData
+
+            pieChartView.setDrawEntryLabels(true)
+            pieChartView.setUsePercentValues(true)
+            pieChartView.description.isEnabled = false
+            pieChartView.centerText = "Seu Portifólio"
+            pieChartView.setExtraOffsets(5F, 10F, 5F, 15F)
+            pieChartView.setCenterTextSize(16F)
+            pieChartView.centerTextRadiusPercent = 50F
+            pieChartView.animateY(1400, Easing.EaseInOutQuad)
+
+            pieChartView.invalidate()
         })
+
+        viewModel.getAllStocks()
+
+
         return root
     }
+
 }
