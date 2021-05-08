5472e01522ba11c59adfc3e5cb303099eb0d9ca3
Alisson <alissonmanfron77@hotmail.com>
2021-05-08 16:56:36 -0300

    - Foi adicionado uma mensagem na tela de Alertas, para quando o usuário não possuir mensagens.

--- app/src/main/java/br/com/alisson/stockpicking/ui/alert/AlertViewModel.kt
+++ app/src/main/java/br/com/alisson/stockpicking/ui/alert/AlertViewModel.kt
@@ -1,13 +1,5 @@
 package br.com.alisson.stockpicking.ui.alert
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AlertViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is alert Fragment"
-    }
-    val text: LiveData<String> = _text
-}
+class AlertViewModel : ViewModel()
