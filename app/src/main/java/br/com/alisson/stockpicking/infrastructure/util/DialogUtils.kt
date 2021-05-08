18c9aca244fb44c7074565d1ab9c914c62b9bc7f
Alisson <alissonmanfron77@hotmail.com>
2021-05-08 16:29:00 -0300

    - Melhora no layout do dialog de cadastro de stock. - Verificação de campo nulo no dialog de cadastro de stock.

--- app/src/main/java/br/com/alisson/stockpicking/infrastructure/util/DialogUtils.kt
+++ app/src/main/java/br/com/alisson/stockpicking/infrastructure/util/DialogUtils.kt
@@ -1,7 +1,13 @@
 package br.com.alisson.stockpicking.infrastructure.util
 
 import android.app.Activity
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import br.com.alisson.stockpicking.R
 import br.com.alisson.stockpicking.data.model.Stock
@@ -14,25 +20,38 @@ class DialogUtils {
             activity.let {
                 val builder = AlertDialog.Builder(it)
 
-                val inflater = activity.layoutInflater;
+                val inflater = activity.layoutInflater
 
                 val layout = inflater.inflate(R.layout.dialog_add_stock, null)
 
                 val editTicker = layout.findViewById<EditText>(R.id.ticker)
                 val editWeight = layout.findViewById<EditText>(R.id.weight)
+                val btnSave = layout.findViewById<Button>(R.id.save)
 
                 builder.setView(layout)
-                    // Add action buttons
-                    .setPositiveButton(
-                        R.string.button_save_stock
-                    ) { p0, p1 ->
+
+                val alert = builder.create()
+                alert.getWindow()?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                alert.setOnShowListener {
+                    btnSave.setOnClickListener {
+
                         val ticker = editTicker.text.toString()
-                        val weight = editWeight.text.toString().toInt()
-                        callback.onclickListener(Stock(null, ticker, weight))
+                        val weight = editWeight.text.toString()
+
+                        if (ticker.isEmpty() || weight.isEmpty()) {
+                            Toast.makeText(
+                                activity,
+                                "Preencha todos os campos!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            callback.onclickListener(Stock(null, ticker, weight.toInt()))
+                            alert.dismiss()
+                        }
                     }
-                builder.create()
-                builder.show()
-            } ?: throw IllegalStateException("Activity cannot be null")
+                }
+                alert.show()
+            }
         }
     }
 }
